51d6727bec3f7f9d2741b21e0daf9cfc5d217f29
bbq-bean <github.com/bbq-bean>
2023-09-24 19:20:09 -0500

    contains duplicate

---
+++ contains_duplicate.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seent = {}
+        for num in nums:
+            if num in seent:
+                return True
+            seent[num] = 1
+            
